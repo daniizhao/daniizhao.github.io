723e06da9d2b23fe4541dfd7d0295f1d161428cc
Daniela Zhao <daniela.zhao4@gmail.com>
2023-08-21 12:18:39 +0200

    set translation & fix header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,7 @@
 import { useTranslation } from 'react-i18next';
 import './Header.css';
+import Icon from '../icon/Icon';
+import { ReactComponent as Logo } from '../../assets/icon/logoDZ.svg';
 
 const Header = () => {
 
@@ -8,15 +10,17 @@ const Header = () => {
   return (
     <div className='headerContainer'>
       <div className='linksContainer'>
-
+        <Icon iconName={'fa-brands fa-linkedin'} color={"var(--primary)"} />
+        <Icon iconName={'fa-brands fa-github'} color={"var(--primary)"} />
+        <Icon iconName={'fa-brands fa-artstation'} color={"var(--primary)"} />
       </div>
       <div className='logoContainer'>
-        
+        <Logo />
       </div>
       <div className='navContainer'>
-        <a>{t('HEADER.ABOUTME')}</a>
-        <a>{t('HEADER.WORK')}</a>
-        <a>{t('HEADER.CONTACT')}</a>
+        <button>{t('HEADER.ABOUTME')}</button>
+        <button>{t('HEADER.WORK')}</button>
+        <button>{t('HEADER.CONTACT')}</button>
       </div>
     </div>
   )
